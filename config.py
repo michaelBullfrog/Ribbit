82ed47b60acbfc77f34b8be61c971c20299caa15
michaelBullfrog <165947213+michaelBullfrog@users.noreply.github.com>
2025-08-05 14:26:54 -0400

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-WEBEX_BOT_ACCESS_TOKEN = os.getenv("WEBEX_BOT_ACCESS_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-INGRAM_CLIENT_ID = os.getenv("INGRAM_CLIENT_ID")
-INGRAM_CLIENT_SECRET = os.getenv("INGRAM_CLIENT_SECRET")
-ZOHO_ACCESS_TOKEN = os.getenv("ZOHO_ACCESS_TOKEN")
